c04724b82c126af5b8de51ae69eed28de1e5e0c3
gokul <gokulviswanathan25@gmail.com>
2025-11-19 17:32:03 -0500

    removed usless code

--- bigram.py
+++ bigram.py
@@ -11,7 +11,6 @@ learning_rate = 1e-2
 device = "cuda" if torch.cuda.is_available() else "cpu"
 eval_iters = 200
 # ------------
-# new feature to add
 torch.manual_seed(1337)
 
 # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
